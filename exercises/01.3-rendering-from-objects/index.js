74f1dedebb75ce5b4306e3a1c97264744ae82752
jgustavoj <gjime011@fiu.edu>
2020-09-01 00:28:29 +0000

    initial commit

--- exercises/01.3-rendering-from-objects/index.js
+++ exercises/01.3-rendering-from-objects/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const customer = {
-	first_name: "Bob",
-	last_name: "Dylan"
+    first_name: "Bob",
+    last_name: "Dylan"
 };
 
 //              your code inside these <div> tags
-const output = <div></div>;
+const output =
+    <div><h1>My name is {customer.first_name}</h1> 
+    <h2>My last name is {customer.last_name}</h2> </div>;
+
 
 //              what     where
 ReactDOM.render(output, document.querySelector("#myDiv"));
